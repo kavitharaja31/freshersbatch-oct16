640042e92d4cc27ac85cd1edc38ffe2cb20631e4
kavitharaja31 <110021926+kavitharaja31@users.noreply.github.com>
2022-07-28 14:31:12 +0530

    Create sample3.js

---
+++ sample3.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+<script type="text/javascript">
+function msg(){
+  alert("Hello");
+}
+</script>
+</head>
+<body>
+hello world
+<form>
+<input type="button"value="click"onclick="mas()"/>
+</form>
+</body>
+</html>
